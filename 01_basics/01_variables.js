d56b7e9cefc18ab5e055c4873048794926f0aec9
Abhishek <abhishek202327@gmail.com>
2025-09-10 16:59:14 +0000

    config code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1234426
+let accountEmail="abhis@goofgl.com"
+var accountPass="1234"
+accountCity="jaipur"
+
+// accountId=2 not allowed
+
+accountEmail="hc,ah.@hamil.cim"
+accountPass="1243"
+acocountCity="delhi"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPass,acocountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use stirct"; //treatt all code as newer version 
+// number range(2 to 2pow53)
+// bigint
+// string=> ""
+// boolean
+// null
+// undefined
+// symbol=>unique
+// object
+let accountEmail="abhis@goofgl.com"
+console.log(typeof accountEmail)
