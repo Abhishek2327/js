b806d49eab047a258832bd5e8ac67293d086bb9e
Abhishek <abhishek202327@gmail.com>
2025-09-11 05:46:22 +0000

    config code

---
+++ 01_basics/03_conersionoperations.js
@@ -0,0 +1,11 @@
+let score=33
+let scorea="33"
+// console.log(typeof scorea) 
+
+let valueInNo=Number(scorea)
+console.log(typeof valueInNo)
+
+// "33"=>33
+// "33abc"=>NaN
+// ******************* operations************//
+
